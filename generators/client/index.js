8b6a147acaad5336faa722e49bf780398ee21372
raymond <nicolas_raymond@orange.fr>
2020-02-01 14:50:14 +0100

    Remove used var

--- generators/client/index.js
+++ generators/client/index.js
@@ -83,7 +83,6 @@ module.exports = class extends ClientGenerator {
                 this.relativeMainClientDir = 'ClientApp';
                 this.relativeMainAngularDir = `${this.relativeMainClientDir}/app`;
                 this.testProjectDir = `${this.pascalizedBaseName}${constants.PROJECT_TEST_SUFFIX}`;
-                this.angularAppName = "";
                 // TODO PROMPT
                 this.authenticationType = 'jwt';
             },
