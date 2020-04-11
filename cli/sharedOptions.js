6dd0af3c39b2b7b7e56c98514d895389cc71639d
raymond <nicolas_raymond@orange.fr>
2020-04-11 09:49:39 +0200

    Remove unused package and file

--- cli/sharedOptions.js
+++
@@ -1,7 +0,0 @@
-//const normalize = require('normalize-path');
-//module.exports = {
-//    outputPathCustomizer: [
-//        paths => (paths ? normalize(paths).replace(/^src\/main\/webapp([\/$])/,  `${this.mainProjectDir}/ClientApp$1`) : paths),
-//        paths => (paths ? normalize(paths).replace(/^src\/main\/webapp$/, `${this.mainProjectDir}/ClientApp`) : paths)
-//    ]
-//};
