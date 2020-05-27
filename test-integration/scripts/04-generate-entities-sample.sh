15725ee15563cca567efff3ae4ed6487d919a950
nicolas63 <nicolas_raymond@orange.fr>
2020-05-27 16:43:39 +0200

    Fix sonar analysis on generated entities

--- test-integration/scripts/04-generate-entities-sample.sh
+++ test-integration/scripts/04-generate-entities-sample.sh
@@ -18,6 +18,12 @@ if [ "$2" = "import-jdl" ]; then
   jhipster $runOptions
   
   if [[ -n $(find src -type f -name "*Employee.cs") ]]; then
+      if "$SONAR_ANALYSE" ; then
+        dotnet tool install --global dotnet-sonarscanner --version 4.9.0
+        dotnet tool install --global coverlet.console
+        dotnet sonarscanner begin /k:"jhipster_jhipster-sample-app-dotnetcore" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
+      fi
+      dotnet build
       echo "${GREEN}GENERATION OK"
   else
       echo "${RED}WRONG GENERATION"
