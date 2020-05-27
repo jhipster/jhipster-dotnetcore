fcd9b497ceafaad5e45b6e65d2b1b93f23e8d83c
nicolas63 <nicolas_raymond@orange.fr>
2020-05-27 17:24:46 +0200

    Fix sonar analysis

--- test-integration/scripts/03-install-dependencies-app-sample.sh
+++ test-integration/scripts/03-install-dependencies-app-sample.sh
@@ -11,12 +11,6 @@ source $(dirname $0)/01-init-env.sh
 #-------------------------------------------------------------------------------
 # Install app dependencies and build 
 #-------------------------------------------------------------------------------
-if "$SONAR_ANALYSE" ; then
-  dotnet tool install --global dotnet-sonarscanner
-  dotnet tool install --global coverlet.console
-  dotnet sonarscanner begin /k:"jhipster_jhipster-sample-app-dotnetcore" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
-fi
-
 echo "*** install dependencies and build : `pwd`"
 dotnet build
 if [ $? -ne 0 ]; then
