b2df3f48b92ae8bf402769d399c796c6c2760d52
nicolas63 <nicolas_raymond@orange.fr>
2020-06-08 23:38:21 +0200

    Fix many to many

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -112,6 +112,7 @@ function writeFiles() {
                             },
                         ],
                     };
+                    this.currentRelation = relationship.joinEntityNamePascalized;
                     this.writeFilesToDisk(files, this, false, 'dotnetcore');
                 }
             });
