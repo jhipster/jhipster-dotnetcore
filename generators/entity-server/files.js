a0846b2f5aee48fc6235c467548adf34fe1bec3d
nicolas63 <nicolas_raymond@orange.fr>
2020-06-09 21:23:32 +0200

    Fix multiple many-to-many relationships in JDL (#242)
    * Added many-to-many relationship in integration test
    
    * Fix many-to-many error

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -22,7 +22,6 @@ const constants = require('../generator-dotnetcore-constants');
 /* Constants use throughout */
 const SERVER_SRC_DIR = constants.SERVER_SRC_DIR;
 const SERVER_TEST_DIR = constants.SERVER_TEST_DIR;
-const joinEntitiesTemplates = [];
 
 const serverFiles = {
     server: [
@@ -53,11 +52,6 @@ const serverFiles = {
                     renameTo: generator => `${generator.mainProjectDir}/Models/RelationshipTools/JoinListFacade.cs`,
                 },
             ],
-        },
-        {
-            condition: generator => generator.entityClassHasManyToMany,
-            path: SERVER_SRC_DIR,
-            templates: joinEntitiesTemplates,
         },
     ],
     db: [
@@ -103,13 +97,25 @@ function writeFiles() {
             this.relationships.forEach(relationship => {
                 // const relationship = relationship;
                 if (relationship.relationshipType === 'many-to-many') {
-                    joinEntitiesTemplates.push({
-                        file: 'Project/Models/JoinEntity.cs',
-                        renameTo: generator => `${generator.mainProjectDir}/Models/${relationship.joinEntityNamePascalized}.cs`,
-                    });
+                    const files = {
+                        server: [
+                            {
+                                condition: generator => generator.entityClassHasManyToMany,
+                                path: SERVER_SRC_DIR,
+                                templates: [
+                                    {
+                                        file: 'Project/Models/JoinEntity.cs',
+                                        renameTo: generator =>
+                                            `${generator.mainProjectDir}/Models/${relationship.joinEntityNamePascalized}.cs`,
+                                    },
+                                ],
+                            },
+                        ],
+                    };
+                    this.currentRelation = relationship.joinEntityNamePascalized;
+                    this.writeFilesToDisk(files, this, false, 'dotnetcore');
                 }
             });
-
             this.writeFilesToDisk(serverFiles, this, false, 'dotnetcore');
         },
     };
