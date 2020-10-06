0cd59c604393996adf70c5b79a9808ffe533ddf1
Enmanuel Reyes <EnmanuelReyes@users.noreply.github.com>
2020-10-06 14:03:36 -0400

    Add swagger-resources endpoint (#412)
    * Add swagger-resources endpoint
    
    * Add SwaggerController to files
    
    * Fix prettier
    
    * Extracting SwaggerResource as a DTO

--- generators/server/files.js
+++ generators/server/files.js
@@ -267,6 +267,15 @@ const serverFiles = {
                 },
             ],
         },
+        {
+            path: SERVER_SRC_DIR,
+            templates: [
+                {
+                    file: 'Project.Dto/SwaggerResourceDto.cs',
+                    renameTo: generator => `${generator.pascalizedBaseName}${constants.PROJECT_DTO_SUFFIX}/SwaggerResourceDto.cs`,
+                },
+            ],
+        },
     ],
     services: [
         {
@@ -878,6 +887,15 @@ const serverFiles = {
         },
     ],
     serverMisc: [
+        {
+            path: SERVER_SRC_DIR,
+            templates: [
+                {
+                    file: 'Project/Controllers/SwaggerController.cs',
+                    renameTo: generator => `${generator.mainProjectDir}/Controllers/SwaggerController.cs`,
+                },
+            ],
+        },
         {
             condition: generator => generator.authenticationType === 'jwt',
             path: SERVER_TEST_DIR,
