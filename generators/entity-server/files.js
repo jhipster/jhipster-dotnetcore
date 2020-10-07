67b3b864c0c0744a02def01792084f344e5760b9
nicolas63 <nicolas_raymond@orange.fr>
2020-10-07 21:17:33 +0200

    Fix double declaration

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -16,9 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-const baseConstants = require('generator-jhipster/generators/generator-constants');
-const constants = require('../generator-dotnetcore-constants');
 const baseConstants = require('generator-jhipster/generators/generator-constants');
 const constants = require('../generator-dotnetcore-constants');
 const utils = require('../utils');
