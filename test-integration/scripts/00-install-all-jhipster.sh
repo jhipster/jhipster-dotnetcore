66a194dac2e7e1d3738aecb9a5e242feb90b41b6
nicolas63 <nicolas_raymond@orange.fr>
2020-10-30 21:24:19 +0100

    Install jhipster from repo

--- test-integration/scripts/00-install-all-jhipster.sh
+++ test-integration/scripts/00-install-all-jhipster.sh
@@ -6,7 +6,7 @@ set -e
 # Install JHipster Generator
 #-------------------------------------------------------------------------------
 echo "*** generator-jhipster: use last version"
-npm install -g generator-jhipster
+npm install -g https://github.com/jhipster/generator-jhipster
 
 #-------------------------------------------------------------------------------
 # Install yeoman
