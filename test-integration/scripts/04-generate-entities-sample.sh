529f65b913c932682886077d7b61cb8d2990f7f9
nicolas63 <nicolas_raymond@orange.fr>
2020-12-06 10:34:26 +0100

    Fix sh

--- test-integration/scripts/04-generate-entities-sample.sh
+++ test-integration/scripts/04-generate-entities-sample.sh
@@ -29,7 +29,7 @@ if [ "$2" = "import-jdl" ]; then
         dotnet tool install --global dotnet-sonarscanner
         dotnet tool install --global coverlet.console
         dotnet sonarscanner begin /k:"jhipster_jhipster-sample-app-dotnetcore" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
-      else if "$SONAR_ANALYSE_BLAZOR" ; then
+      elif "$SONAR_ANALYSE_BLAZOR" ; then
         dotnet tool install --global dotnet-sonarscanner
         dotnet tool install --global coverlet.console
         dotnet sonarscanner begin /k:"jhipster_jhipster_sample-app-blazor" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
