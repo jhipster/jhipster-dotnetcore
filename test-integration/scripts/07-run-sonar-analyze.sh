604b73f250620a447f5ac0cac1148e54cbf19926
nicolas63 <nicolas_raymond@orange.fr>
2020-12-06 20:48:52 +0100

    Added sonar analyze on blazor (#480)

--- test-integration/scripts/07-run-sonar-analyze.sh
+++ test-integration/scripts/07-run-sonar-analyze.sh
@@ -9,7 +9,7 @@ source $(dirname $0)/01-init-env.sh
 #-------------------------------------------------------------------------------
 # Run Sonar  
 #-------------------------------------------------------------------------------
-if "$SONAR_ANALYSE" ; then
+if [[ "$SONAR_ANALYSE_ANGULAR" = true || "$SONAR_ANALYSE_BLAZOR" = true ]] ; then
   echo "*** run sonar analyze in server for : `pwd`"
   dotnet sonarscanner end /d:sonar.login=$SONAR_TOKEN
 fi
