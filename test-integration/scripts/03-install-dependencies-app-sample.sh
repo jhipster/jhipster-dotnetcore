6de46a257b6102e17e7021237f7d6327fbc847ea
Zenrac <enzo.carnezpandolfi@cgi.com>
2021-07-16 16:24:46 +0200

    Add possibility to use MongoDB as database (#717)
    Add possibility to use MongoDB as database (#717)

--- test-integration/scripts/03-install-dependencies-app-sample.sh
+++ test-integration/scripts/03-install-dependencies-app-sample.sh
@@ -9,13 +9,13 @@ GREEN='\033[0;32m'
 source $(dirname $0)/01-init-env.sh
 
 #-------------------------------------------------------------------------------
-# Install app dependencies and build 
+# Install app dependencies and build
 #-------------------------------------------------------------------------------
 echo "*** install dependencies and build : `pwd`"
 
 dotnet build
 
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
   echo "${RED}FAILED INSTALL DEPENDENCIES OR BUILD"
   exit 1
 fi
--- test-integration/scripts/04-generate-entities-sample.sh
+++ test-integration/scripts/04-generate-entities-sample.sh
@@ -7,14 +7,18 @@ GREEN='\033[0;32m'
 
 source $(dirname $0)/01-init-env.sh
 
-if [ "$2" = "import-jdl" ]; then
+if [[ "$2" = "import-jdl" ]]; then
   #-------------------------------------------------------------------------------
   # Run JHipster.NET Generator
   #-------------------------------------------------------------------------------
   echo "*** run generation app with dotnetcore blueprint for : "$APP_FOLDER
   runOptions="--blueprints dotnetcore --skip-checks --force --no-insight --skip-install"
 
-  runOptions="import-jdl ../jdl-default/app.jdl $runOptions"
+  if [[ "$IS_MONGO" ]] ; then
+      runOptions="import-jdl ../jdl-default/app_mongo.jdl $runOptions"
+  else
+      runOptions="import-jdl ../jdl-default/app.jdl $runOptions"
+  fi
   jhipster $runOptions
 
   # copy files required for testing automatic service class/interface registration on dependency injection container
@@ -25,11 +29,11 @@ if [ "$2" = "import-jdl" ]; then
   fi
 
   if [[ -n $(find src/JhipsterSampleApplication.Domain -type f -name "*Employee.cs") ]]; then
-      if "$SONAR_ANALYSE_ANGULAR" ; then
+      if [[ "$SONAR_ANALYSE_ANGULAR" ]] ; then
         dotnet tool install --global dotnet-sonarscanner
         dotnet tool install --global coverlet.console
         dotnet sonarscanner begin /k:"jhipster_jhipster-sample-app-dotnetcore" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
-      elif "$SONAR_ANALYSE_BLAZOR" ; then
+      elif [[ "$SONAR_ANALYSE_BLAZOR" ]] ; then
         dotnet tool install --global dotnet-sonarscanner
         dotnet tool install --global coverlet.console
         dotnet sonarscanner begin /k:"jhipster_jhipster-sample-app-blazor" /o:"jhipster" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN /s:"`pwd`/SonarQube.Analysis.xml"
