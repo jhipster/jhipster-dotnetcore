dcc9d7bf9dd93f86c8060d2efed1410ad653aa3d
Zenrac <enzo.carnezpandolfi@cgi.com>
2021-07-16 16:24:46 +0200

    Add possibility to use MongoDB as database (#717)
    Add possibility to use MongoDB as database (#717)

--- test-integration/scripts/09-verify-format.sh
+++ test-integration/scripts/09-verify-format.sh
@@ -7,12 +7,12 @@ RED='\033[0;31m'
 source $(dirname $0)/01-init-env.sh
 
 #-------------------------------------------------------------------------------
-# Run FORMATTING  
+# Run FORMATTING
 #-------------------------------------------------------------------------------
 dotnet tool install -g dotnet-format
 dotnet format --check
 
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
   echo "${RED}FAILED FORMATTING"
   exit 1
 fi
