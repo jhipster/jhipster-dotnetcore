7fb6db0b59170aa1bb81b55f726bdaffbe5c926a
nicolas63 <nicolas_raymond@orange.fr>
2022-03-08 15:47:55 +0100

    Release 3.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Nicolas Raymond'
 author = 'Nicolas Raymond'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.1'
+release = '3.2.0'
 
 
 # -- General configuration ---------------------------------------------------
