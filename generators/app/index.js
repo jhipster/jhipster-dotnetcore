8985b74a4b4dcdd94b2253e30c33eb89e0fd3b61
eugen.popa <eugen.g.popa@gmail.com>
2023-03-28 13:10:54 +0300

    remove unused import

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable consistent-return */
 const chalk = require('chalk');
 const AppGenerator = require('generator-jhipster/generators/app');
-const { GENERATOR_CI_CD } = require('generator-jhipster/generators/generator-list');
 const packagejs = require('../../package.json');
 const prompts = require('./prompts');
 
