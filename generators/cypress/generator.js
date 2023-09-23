0644e376d265a7fb2bd9051237f909d3eea30518
Marcelo Shima <marceloshima@gmail.com>
2023-09-23 07:32:28 -0300

    rework cypress

--- generators/cypress/generator.js
+++ generators/cypress/generator.js
@@ -1,66 +1,40 @@
-const chalk = require('chalk');
-const CypressGenerator = require('generator-jhipster/generators/cypress');
-const writeCypressFiles = require('./files-cypress').writeFiles;
-const customizeDotnetPaths = require('../utils').customizeDotnetPaths;
+import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+import { writeFiles } from './files-cypress.js';
 
-module.exports = class extends CypressGenerator {
-  constructor(args, opts) {
-    super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
-
-    const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
-
-    if (!jhContext) {
-      this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint dotnetcore')}`);
-    }
-
-    if (this.configOptions.baseName) {
-      this.baseName = this.configOptions.baseName;
-    }
+export default class extends BaseApplicationGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, sbsBlueprint: true });
   }
 
-  get initializing() {
-    return super._initializing();
-  }
-
-  get configuring() {
-    return {
-      customizeDotnetPaths,
-      ...super._configuring(),
-    };
-  }
-
-  get composing() {
-    return super._composing();
-  }
-
-  get loading() {
-    return {
-      ...super._loading(),
-      loadingDotnet() {
-        this.serverPort = this.jhipsterConfig.serverPort;
-        this.serverPortSecured = parseInt(this.serverPort, 10) + 1;
+  get [BaseApplicationGenerator.PREPARING]() {
+    return this.asPreparingTaskGroup({
+      preparing({ application }) {
+        application.cypressDir = `${SERVER_SRC_DIR}${application.clientTestProject}/cypress/`;
+        application.serverPortSecured = parseInt(application.serverPort, 10) + 1;
       },
-    };
+    });
   }
 
-  get preparing() {
-    return super._preparing();
-  }
-
-  get default() {
-    return super._default();
-  }
-
-  get writing() {
-    return super._writing();
-  }
-
-  get postWriting() {
-    return {
-      ...super._postWriting(),
-      postWriteFilesDotnetcore() {
-        return writeCypressFiles.call(this);
+  get [BaseApplicationGenerator.POST_WRITING]() {
+    return this.asPostWritingTaskGroup({
+      removeNotImplementedFeatureInCypress({ application }) {
+        this.deleteDestination(`${application.cypressDir}integration/account/reset-password-page.spec.ts`);
+        this.deleteDestination(`${application.cypressDir}integration/administration/administration.spec.ts`);
       },
-    };
+
+      updateTsConfigCypress() {
+        this.editFile(`${application.cypressDir}/tsconfig.json`, content => content.replace('./../../../../', './../../'));
+      },
+
+      updateCypressJson() {
+        this.editFile(`${SERVER_SRC_DIR}${application.mainClientDir}/cypress.json`, content =>
+          content.replace(`${SERVER_SRC_DIR}${application.mainClientDir}/`, ''),
+        );
+        this.editFile(`${SERVER_SRC_DIR}${application.mainClientDir}/cypress.json`, content =>
+          content.replace(`http://localhost:${application.serverPort}`, `https://localhost:${application.serverPortSecured}`),
+        );
+        this.editFile(`${SERVER_SRC_DIR}${application.mainClientDir}/cypress.json`, content => content.replace('target', 'dist'));
+      },
+    });
   }
-};
+}
