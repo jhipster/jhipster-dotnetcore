72a8e1270d8dc6d6cedcd5f665fce6db30b92d65
Marcelo Shima <marceloshima@gmail.com>
2023-09-23 09:47:29 -0300

    drop files-angular

--- generators/angular/generator.js
+++ generators/angular/generator.js
@@ -7,7 +7,22 @@ export default class extends BaseApplicationGenerator {
 
   get [BaseApplicationGenerator.POST_WRITING]() {
     return this.asPostWritingTaskGroup({
-      async postWritingTemplateTask() {},
+      async postWritingTemplateTask() {
+        /*
+
+  this.replaceContent(`${SERVER_SRC_DIR}${this.mainClientDir}/angular.json`, `${SERVER_SRC_DIR}${this.mainClientDir}/`, '', true);
+  this.replaceContent(`${SERVER_SRC_DIR}${this.mainClientDir}/angular.json`, `target/classes/static/`, 'dist/', true);
+
+  this.replaceContent(`${SERVER_SRC_DIR}${this.mainClientAppDir}/app/home/home.component.html`, 'Java', '.Net Core', false);
+
+  this.replaceContent(
+    `${SERVER_SRC_DIR}${this.mainClientDir}/webpack/webpack.custom.js`,
+    `${SERVER_SRC_DIR}${this.mainClientDir}/`,
+    '',
+    true,
+  );
+        */
+      },
     });
   }
 }
