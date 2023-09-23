f3a352ad4609ccc5f2e68b41336a84dc09ab8c6d
Marcelo Shima <marceloshima@gmail.com>
2023-09-22 22:15:18 -0300

    add tests

---
+++ generators/heroku/generator.spec.js
@@ -0,0 +1,26 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+
+import { defaultHelpers as helpers, result } from 'generator-jhipster/testing';
+
+const SUB_GENERATOR = 'heroku';
+const BLUEPRINT_NAMESPACE = `jhipster:${SUB_GENERATOR}`;
+
+describe('SubGenerator heroku of dotnetcore JHipster blueprint', () => {
+  describe('run', () => {
+    beforeAll(async function () {
+      await helpers
+        .run(BLUEPRINT_NAMESPACE)
+        .withJHipsterConfig()
+        .withOptions({
+          ignoreNeedlesError: true,
+          blueprint: 'dotnetcore',
+        })
+        .withJHipsterLookup()
+        .withParentBlueprintLookup();
+    });
+
+    it('should succeed', () => {
+      expect(result.getStateSnapshot()).toMatchSnapshot();
+    });
+  });
+});
