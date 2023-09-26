554f29de95700556045840215d93fb634169dc05
Marcelo Shima <marceloshima@gmail.com>
2023-09-26 17:54:34 -0300

    adjusts

--- generators/cypress/generator.js
+++ generators/cypress/generator.js
@@ -1,5 +1,4 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
-import { SERVER_SRC_DIR } from '../generator-dotnetcore-constants.js';
 
 export default class extends BaseApplicationGenerator {
   constructor(args, opts, features) {
@@ -8,14 +7,6 @@ export default class extends BaseApplicationGenerator {
 
   async beforeQueue() {
     await this.dependsOnJHipster('jhipster-dotnetcore:bootstrap-dotnetcore');
-  }
-
-  get [BaseApplicationGenerator.PREPARING]() {
-    return this.asPreparingTaskGroup({
-      preparing({ application }) {
-        // application.cypressDir = `${SERVER_SRC_DIR}${application.clientTestProject}/cypress/`;
-      },
-    });
   }
 
   get [BaseApplicationGenerator.POST_WRITING]() {
