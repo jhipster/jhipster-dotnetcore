de3a8c50900c95b7ddb4bbb56346453f4381a984
Marcelo Shima <marceloshima@gmail.com>
2023-09-26 22:44:32 -0300

    adjusts to client generator

--- generators/client/generator.js
+++ generators/client/generator.js
@@ -1,19 +1,24 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+import command from './command.js';
+import { BLAZOR, XAMARIN } from '../generator-dotnetcore-constants.js';
 
 export default class extends BaseApplicationGenerator {
   constructor(args, opts, features) {
     super(args, opts, { ...features, sbsBlueprint: true });
-  }
 
-  get [BaseApplicationGenerator.PROMPTING]() {
-    return this.asPromptingTaskGroup({
-      async promptingTemplateTask() {},
-    });
+    this.jhipsterContext.command = command;
   }
 
   get [BaseApplicationGenerator.COMPOSING]() {
     return this.asComposingTaskGroup({
-      async composingTemplateTask() {},
+      async composingTemplateTask() {
+        if (this.jhipsterConfig.clientFramework === BLAZOR) {
+          await this.composeWithJHipster('jhipster-dotnetcore:blazor');
+        }
+        if (this.jhipsterConfig.clientFramework === XAMARIN) {
+          await this.composeWithJHipster('jhipster-dotnetcore:xamarin');
+        }
+      },
     });
   }
 }
