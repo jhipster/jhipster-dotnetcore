4e09161dc29ffb1ffcc9f1f48fcb9a72df194f3e
Marcelo Shima <marceloshima@gmail.com>
2023-09-27 19:39:08 -0300

    skip some cypress tests

--- generators/cypress/generator.js
+++ generators/cypress/generator.js
@@ -14,13 +14,19 @@ export default class extends BaseApplicationGenerator {
       removeNotImplementedFeatureInCypress({ application }) {
         this.deleteDestination(`${application.cypressDir}e2e/account/reset-password-page.cy.ts`);
         this.deleteDestination(`${application.cypressDir}e2e/administration/administration.cy.ts`);
-      },
-      updateCypressJson({ application }) {
-        /*
-        this.editFile(`${application.clientAppRootDir}cypress.config.ts`, content =>
-          content.replace(`http://localhost:${application.serverPort}`, `https://localhost:${application.serverPortSecured}`),
+
+        this.editFile(`${application.cypressDir}e2e/account/settings-page.cy.ts`, content =>
+          content
+            .replace(
+              `it("should be able to change 'user' firstname settings"`,
+              `it.skip("should be able to change 'user' firstname settings"`,
+            )
+            .replace(
+              `it("should be able to change 'user' lastname settings"`,
+              `it.skip("should be able to change 'user' lastname settings"`,
+            )
+            .replace(`it("should be able to change 'user' email settings"`, `it.skip("should be able to change 'user' email settings"`),
         );
-        */
       },
     });
   }
