f5caaeb834979cf70a2a52de02c6de7b8b1bb6c2
Marcelo Shima <marceloshima@gmail.com>
2023-09-27 21:46:43 -0300

    cleanup

--- generators/common/generator.js
+++ generators/common/generator.js
@@ -12,28 +12,6 @@ export default class extends BaseApplicationGenerator {
 
   get [BaseApplicationGenerator.WRITING]() {
     return this.asWritingTaskGroup({
-      /*
-      configureGlobal() {
-        this.kebabCasedBaseName = _.kebabCase(this.baseName);
-        this.pascalizedBaseName = toPascalCase(this.baseName);
-        this.mainProjectDir = this.pascalizedBaseName;
-        this.mainClientDir = `${this.mainProjectDir}/ClientApp`;
-        this.jhipsterDotnetVersion = packagejs.version;
-        this.options.outputPathCustomizer = [
-          paths => (paths ? paths.replace(/^(\.prettierignore)$/, `src/${this.mainClientDir}/$1`) : paths),
-          paths => (paths ? paths.replace(/^(\.prettierrc)$/, `src/${this.mainClientDir}/$1`) : paths),
-          paths => (paths ? paths.replace(/^(package.json)$/, `src/${this.mainClientDir}/$1`) : paths),
-        ];
-      },
-      */
-      /*
-      async writingCommonFiles({ application }) {
-        await this.writeFiles({
-          sections: jhipsterCommonFiles,
-          context: application,
-        });
-      },
-      */
       async writingDotNetFiles({ application }) {
         await this.writeFiles({
           sections: files,
