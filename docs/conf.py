32df308bdb2e1d6d800fbc5b55c2fc19b271b1c2
nicolas63 <nicolas_raymond@orange.fr>
2023-11-26 11:01:29 +0100

    Release 4.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Nicolas Raymond'
 author = 'Nicolas Raymond'
 
 # The full version, including alpha/beta/rc tags
-release = '3.9.1'
+release = '4.0.0'
 
 
 # -- General configuration ---------------------------------------------------
