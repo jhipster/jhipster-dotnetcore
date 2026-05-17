cc42a685f490e938b72afa25d06ed1a78a7af775
Abu1982 <mlwinuk@gmail.com>
2026-05-18 07:48:39 +0800

    fix: format files flagged by Prettier

--- generators/entity/support/index.mjs
+++ generators/entity/support/index.mjs
@@ -2,11 +2,15 @@ export function getRelationships(entity) {
   const relationships = entity.relationships || [];
   return relationships.map((rel) => ({
     ...rel,
-    otherEntityNameCapitalized: rel.otherEntityName.charAt(0).toUpperCase() + rel.otherEntityName.slice(1),
+    otherEntityNameCapitalized:
+      rel.otherEntityName.charAt(0).toUpperCase() +
+      rel.otherEntityName.slice(1),
     otherEntityFieldCapitalized: rel.otherEntityField
-      ? rel.otherEntityField.charAt(0).toUpperCase() + rel.otherEntityField.slice(1)
-      : 'Id',
-    relationshipNamePlural: rel.relationshipNamePlural || `${rel.relationshipName}s`,
+      ? rel.otherEntityField.charAt(0).toUpperCase() +
+        rel.otherEntityField.slice(1)
+      : "Id",
+    relationshipNamePlural:
+      rel.relationshipNamePlural || `${rel.relationshipName}s`,
   }));
 }
 
